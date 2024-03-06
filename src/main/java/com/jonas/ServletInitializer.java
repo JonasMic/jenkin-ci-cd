5eeed2f6585b029aceed95fa9e10d6d2924a4a92
Yonas <yandemicael@gmail.com>
2024-03-05 21:03:48 -0500

    updated

--- src/main/java/com/jonas/ServletInitializer.java
+++ src/main/java/com/jonas/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(com.javatechie.JenkinsCiCdApplication.class);
+		return application.sources(com.jonas.JenkinsCiCdApplication.class);
 	}
 
 }
